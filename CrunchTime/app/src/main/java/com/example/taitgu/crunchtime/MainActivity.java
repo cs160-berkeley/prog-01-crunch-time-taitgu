bce27ca6624167eb69747ce7603f0549ec1139f7
Tait Gu <taitgu@berkeley.edu>
2016-02-05 00:36:40 -0800

    finished core functions

--- CrunchTime/app/src/main/java/com/example/taitgu/crunchtime/MainActivity.java
+++ CrunchTime/app/src/main/java/com/example/taitgu/crunchtime/MainActivity.java
@@ -10,6 +10,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.widget.EditText;
+import android.text.TextWatcher;
+import android.text.Editable;
+
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -17,13 +21,18 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText calories;
+    private EditText pushups;
+    private EditText situps;
+    private EditText jumpingJacks;
+    private EditText jogging;
+    private boolean changeFlag;
     private static final HashMap<String, Double> exerciseValues;
 
     static {
         exerciseValues = new HashMap<String, Double>();
-        exerciseValues.put("pushup", 3.5);
-        exerciseValues.put("situp", 2.0);
+        exerciseValues.put("pushups", 3.5);
+        exerciseValues.put("situps", 2.0);
         exerciseValues.put("jumping jacks", 0.1);
         exerciseValues.put("jogging", 0.12);
     }
@@ -52,7 +61,116 @@ public class MainActivity extends AppCompatActivity {
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+
+        //user code
+        changeFlag = true;
+        calories = (EditText)findViewById(R.id.calories);
+        calories.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                if (changeFlag) {
+                    if (s.toString().equals("")) {
+                        clearValues();
+                    } else {
+                        updateValues(Double.parseDouble(s.toString()));
+                        calories.setSelection(calories.getText().length());
+                    }
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
+
+
+        pushups = (EditText)findViewById(R.id.pushups);
+        pushups.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                if(changeFlag) {
+                    if(s.toString().equals("")){
+                        clearValues();
+                    } else {
+                        updateValues(Double.parseDouble(s.toString()) / exerciseValues.get("pushups"));
+                        pushups.setSelection(pushups.getText().length());
+                    }
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
+
+        situps = (EditText)findViewById(R.id.situps);
+        situps.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                if(changeFlag) {
+                    if(s.toString().equals("")){
+                        clearValues();
+                    } else {
+                        updateValues(Double.parseDouble(s.toString()) / exerciseValues.get("situps"));
+                        situps.setSelection(situps.getText().length());
+                    }
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
+
+        jumpingJacks = (EditText)findViewById(R.id.jumpingJacks);
+        jumpingJacks.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                if(changeFlag) {
+                    if(s.toString().equals("")){
+                        clearValues();
+                    } else {
+                        updateValues(Double.parseDouble(s.toString()) / exerciseValues.get("jumping jacks"));
+                        jumpingJacks.setSelection(jumpingJacks.getText().length());
+                    }
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
+
+        jogging = (EditText)findViewById(R.id.jogging);
+        jogging.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                if(changeFlag) {
+                    if(s.toString().equals("")){
+                        clearValues();
+                    } else {
+                        updateValues(Double.parseDouble(s.toString()) / exerciseValues.get("jogging"));
+                        jogging.setSelection(jogging.getText().length());
+                    }
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
     }
+
+    private void updateValues(Double amount){
+        if  (changeFlag) {
+            changeFlag = false;
+            calories.setText(String.valueOf(java.lang.Math.round(amount)));
+            pushups.setText(String.valueOf(java.lang.Math.round(amount * exerciseValues.get("pushups"))));
+            situps.setText(String.valueOf(java.lang.Math.round(amount * exerciseValues.get("situps"))));
+            jumpingJacks.setText(String.valueOf(java.lang.Math.round(amount * exerciseValues.get("jumping jacks"))));
+            jogging.setText(String.valueOf(java.lang.Math.round(amount * exerciseValues.get("jogging"))));
+            changeFlag = true;
+        }
+    }
+
+    private void clearValues(){
+        if (changeFlag) {
+            changeFlag = false;
+            calories.getText().clear();
+            pushups.getText().clear();
+            situps.getText().clear();
+            jumpingJacks.getText().clear();
+            jogging.getText().clear();
+            changeFlag = true;
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
